b59a4118fb7dd8dbdfdbe0d6c79f0755c2285cf8
Brian Sayler <sayler.b@gmail.com>
2017-08-13 17:35:12 -0500

    initial commit :tada:

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './pomo-timer.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>The Best Pomodooo</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
